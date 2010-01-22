bc7972b5ac7e5098a42382a4ac84f5fddd8cc635
Blake Watters <blake@objective3.com>
2010-01-21 21:13:59 -0500

    Added first version of UISpec CLI runner

--- Specs/main.m
+++ Specs/main.m
@@ -13,7 +13,13 @@ int main(int argc, char *argv[]) {
     
 	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
 	
-	[UISpec runSpecs];
+	NSString* specToRun = [NSString stringWithFormat:@"%s", getenv("UISPEC_RUN_SPEC")];
+	NSLog(@"Running spec with name: %@", specToRun);
+	if (NO == [specToRun isEqualToString:@""]) {
+		[UISpec runSpec:specToRun afterDelay:1];
+	} else {
+		[UISpec runSpecs];
+	}	
 	
     int retVal = UIApplicationMain(argc, argv, nil, nil);
     [pool release];
