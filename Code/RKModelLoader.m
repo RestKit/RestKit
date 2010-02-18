b018819a49052ce4617ad1306b96e6f05ec8b79f
Blake Watters <blake@objective3.com>
2010-02-18 15:18:27 -0500

    Threaded the model loader. Parsing operations occur in a background thread and then the results are returned to the main thread

--- Code/RKModelLoader.m
+++ Code/RKModelLoader.m
@@ -29,17 +29,17 @@
 	return @selector(loadModelsFromResponse:);
 }
 
-- (BOOL)processResponse:(RKResponse*)response {
+- (BOOL)encounteredErrorWhileProcessingRequest:(RKResponse*)response {
 	NSString* errorMessage = nil;
 	RKRequest* request = response.request;
 	if ([response isFailure]) {
 		[_delegate modelLoaderRequest:response.request didFailWithError:response.failureError response:response model:(id<RKModelMappable>)request.userData];
 		return YES;
 	} else if ([response isClientError]) {
+		// TODO: These assumptions are around how Rails serializes error into a payload. Factor out into an adapter...
 		if ([response isXML]) {
 			errorMessage = [[(Element*)[response payloadXMLDocument] selectElement:@"error"] contentsText];
 		} else if ([response isJSON]) {
-			// TODO - Need to test!!!
 			errorMessage = [[response payloadJSONDictionary] objectForKey:@"error"];
 		}
 		if (nil == errorMessage) {
@@ -56,19 +56,30 @@
 	return NO;
 }
 
+- (void)processLoadModelInBackground:(RKResponse*)response {
+	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+	id model = [_mapper buildModelFromString:[response payloadString]];
+	[_delegate performSelectorOnMainThread:self.callback withObject:model waitUntilDone:NO];
+	[pool release];
+}
+
 - (void)loadModelFromResponse:(RKResponse*)response {
-	if (NO == [self processResponse:response] && [response isSuccessful]) {
-		id model = [_mapper buildModelFromString:[response payloadString]];	
-		[_delegate performSelector:self.callback withObject:model];
-	}
-	// TODO - What do we do???
+	if (NO == [self encounteredErrorWhileProcessingRequest:response] && [response isSuccessful]) {		
+		[self performSelectorInBackground:@selector(processLoadModelInBackground:) withObject:response];
+	}	
+}
+
+- (void)processLoadModelsInBackground:(RKResponse *)response {
+	NSAutoreleasePool* pool = [[NSAutoreleasePool alloc] init];
+	NSArray* models = [_mapper buildModelsFromString:[response payloadString]];
+	[_delegate performSelectorOnMainThread:self.callback withObject:models waitUntilDone:NO];
+	[pool release];
 }
 
 - (void)loadModelsFromResponse:(RKResponse*)response {
-	if (NO == [self processResponse:response] && [response isSuccessful]) {
-		NSArray* models = [_mapper buildModelsFromString:[response payloadString]];
-		[_delegate performSelector:self.callback withObject:models];
-	}
+	if (NO == [self encounteredErrorWhileProcessingRequest:response] && [response isSuccessful]) {		
+		[self performSelectorInBackground:@selector(processLoadModelsInBackground:) withObject:response];
+	}	
 }
 
 @end
