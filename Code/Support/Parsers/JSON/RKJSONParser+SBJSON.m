89f26fbe7f889e2d5b6b36e454ea1a55c5107c4e
Jeff Arena <jeff@gateguruapp.com>
2010-10-19 10:28:34 -0400

    fix use of released object in debug message by relocating the release call

--- Code/Support/Parsers/JSON/RKJSONParser+SBJSON.m
+++ Code/Support/Parsers/JSON/RKJSONParser+SBJSON.m
@@ -14,11 +14,11 @@
 - (NSDictionary*)objectFromString:(NSString*)string {
 	SBJsonParser* parser = [[SBJsonParser alloc] init];
 	id result = [parser objectWithString:string];
-	[parser release];
 	if (nil == result) {
 		// TODO: Need to surface these errors in a better fashion
 		NSLog(@"[RestKit] RKMappingFormatJSONParser: Parser failed with error trace: %@ and string: %@", [parser errorTrace], string);
 	}
+	[parser release];
 	
 	return result;
 }
