7cb37bcced2df3c61f89cbdc7043b06fda7744b4
daniel <daniel@twotoasters.com>
2011-01-07 12:03:51 -0500

    iPhone project init

---
+++ Examples/RKDiscussionBoardExample/DiscussionBoard/Classes/DiscussionBoardAppDelegate.h
@@ -0,0 +1,18 @@
+//
+//  DiscussionBoardAppDelegate.h
+//  DiscussionBoard
+//
+//  Created by Daniel Hammond on 1/7/11.
+//  Copyright 2011 Two Toasters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiscussionBoardAppDelegate : NSObject <UIApplicationDelegate> {
+    UIWindow *window;
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
---
+++ Examples/RKDiscussionBoardExample/DiscussionBoard/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  DiscussionBoard
+//
+//  Created by Daniel Hammond on 1/7/11.
+//  Copyright 2011 Two Toasters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
