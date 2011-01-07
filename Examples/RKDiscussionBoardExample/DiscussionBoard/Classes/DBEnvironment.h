3d402a2c75815ce12a94d05e798d3181ed10c65b
daniel <daniel@twotoasters.com>
2011-01-07 12:54:00 -0500

    Created and included constants file

---
+++ Examples/RKDiscussionBoardExample/DiscussionBoard/Classes/DBEnvironment.h
@@ -0,0 +1,13 @@
+//
+//  DBEnvironment.h
+//  DiscussionBoard
+//
+//  Created by Daniel Hammond on 1/7/11.
+//  Copyright 2011 Two Toasters. All rights reserved.
+//
+
+/**
+ * General Constants
+ */
+
+extern NSString* const kDBBaseURLString;
---
+++ Examples/RKDiscussionBoardExample/DiscussionBoard/Classes/DBEnvironment.m
@@ -0,0 +1,11 @@
+//
+//  DBEnvironment.m
+//  DiscussionBoard
+//
+//  Created by Daniel Hammond on 1/7/11.
+//  Copyright 2011 Two Toasters. All rights reserved.
+//
+
+#import "DBEnvironment.h"
+
+NSString* const kDBBaseURLString = @"http://localhost:3000";
