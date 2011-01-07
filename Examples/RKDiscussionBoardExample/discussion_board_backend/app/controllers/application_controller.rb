f6b7122b023d1f6f2f05f0906e949031fb2fc3ab
Jeremy Ellison <jeremy@twotoasters.com>
2011-01-07 12:28:51 -0500

    CRUD for Topics

--- Examples/RKDiscussionBoardExample/discussion_board_backend/app/controllers/application_controller.rb
+++ Examples/RKDiscussionBoardExample/discussion_board_backend/app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  protected
+  
+  def require_user
+    # Look for the correct headers
+    token = request.headers["HTTP_USER_ACCESS_TOKEN"]
+    @user = User.find_by_single_access_token(token)
+    if @user.nil?
+      render :json => {:error => "Invalid Access Token"}
+    end
+  end
+  
 end
