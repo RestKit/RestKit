0f681adf79c7c11a474cd6957ab1ec8600c13d5e
Greg Combs <gcombs@gmail.com>
2011-03-06 16:47:19 -0800

    extra error checking

--- Code/CoreData/RKManagedObjectSeeder.h
+++ Code/CoreData/RKManagedObjectSeeder.h
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 //
 //  RKManagedObjectSeeder.h
 //  RestKit
@@ -67,50 +66,3 @@ extern NSString* const RKDefaultSeedDatabaseFileName;
 - (void)finalizeSeedingAndExit;
 
 @end
-=======
-//
-//  RKObjectSeeder.h
-//  RestKit
-//
-//  Created by Blake Watters on 3/4/10.
-//  Copyright 2010 Two Toasters. All rights reserved.
-//
-
-#import "../ObjectMapping/ObjectMapping.h"
-
-// TODO: This class needs an API scrubbing
-// TODO: Should be updated with ability to auto-detect MIME type
-// from the file extension. Does this need a delegate property?
-@interface RKObjectSeeder : NSObject {
-	RKObjectManager* _manager;
-}
-
-/**
- * Initialize a new object seeder
- */
-- (id)initWithObjectManager:(RKObjectManager*)manager;
-
-/**
- * Read a file from the main bundle and seed the database with its contents.
- * Returns the array of model objects built from the file.
- */
-- (NSArray*)seedDatabaseWithBundledFile:(NSString*)fileName ofType:(NSString*)type;
-
-/**
- * Seeds the database with an array of files of the specified type
- */
-- (void)seedDatabaseWithBundledFiles:(NSArray*)fileNames ofType:(NSString*)type;
-
-/**
- * Seed a specific object class with data from a file
- */
-- (void)seedObjectsFromFile:(NSString*)fileName ofType:(NSString*)type toClass:(Class)theClass keyPath:(NSString*)keyPath;
-
-/**
- * Completes a seeding session by persisting the store, outputing an informational message
- * and exiting the process
- */
-- (void)finalizeSeedingAndExit;
-
-@end
->>>>>>> .r450
