992bde4afbcc409acb8d4cecb68f6062ae0ef8c9
Blake Watters <blakewatters@gmail.com>
2011-04-26 10:04:35 -0400

    Implemented spec for mapping NSArray of values to a property via elementToPropertyMappings

--- Specs/ObjectMapping/RKObjectMapperSpec.m
+++ Specs/ObjectMapping/RKObjectMapperSpec.m
@@ -28,6 +28,7 @@
     NSDate* updatedAt;
     NSString* image;
     NSString* email;
+    NSArray* interests;
 }
 
 @property (nonatomic, retain) NSNumber* userID;
@@ -38,12 +39,13 @@
 @property (nonatomic, retain) NSDate* updatedAt;
 @property (nonatomic, retain) NSString* image;
 @property (nonatomic, retain) NSString* email;
+@property (nonatomic, retain) NSArray* interests;
 
 @end
 
 @implementation RKObjectMapperSpecUser
 
-@synthesize userID, encryptedPassword, name, salt, createdAt, updatedAt, image, email;
+@synthesize userID, encryptedPassword, name, salt, createdAt, updatedAt, image, email, interests;
 
 + (NSDictionary*)elementToPropertyMappings {
     return [NSDictionary dictionaryWithKeysAndObjects:
@@ -55,6 +57,7 @@
             @"updated_at", @"updatedAt",
             @"image", @"image",
             @"email", @"email",
+            @"interests", @"interests",
             nil];
 }
 
@@ -225,24 +228,23 @@
     [expectThat(user.email) should:be(nil)];
 }
 
+- (NSString*)arrayOfStringsJSON {
+    return @"{\"user\":"
+    @"{\"encrypted_password\":\"68dad82a867c4a61719fec594c119188ed35cd3b7d42eed1647e46d85f2ffdd8\",\"name\":\"mimi\","
+    @"\"salt\":\"809c79c24cbebfe3f9feea2e9bf98255e5af0f0b8514b6c0d71dcc63fa083688\",\"created_at\":\"2011-02-16T23:04:22Z\","
+    @"\"updated_at\":\"2011-02-16T23:04:22Z\",\"id\":"
+    @"10,\"interests\":[\"basketball\", \"soccer\"]}}";
+}
+
 - (void)itShouldMapAnArrayOfStringsToAProperty {
-/*
- 'tags' =>
- array (
- 'url' => 'http://localhost/~David/gallery3/rest/item_tags/1?
- output=html',
- 'members' =>
- array (
- 0 => 'http://localhost/~David/gallery3/rest/tag_item/379,1?
- output=html',
- 1 => 'http://localhost/~David/gallery3/rest/tag_item/380,1?
- output=html',
- 2 => 'http://localhost/~David/gallery3/rest/tag_item/381,1?
- output=html',
- ),
- )
-*/
-    // TODO: IMPLEMENT ME
+    RKObjectMapperSpecUser* user = [RKObjectMapperSpecUser object];
+    RKObjectMapper* mapper = [[RKObjectMapper alloc] init];
+	mapper.format = RKMappingFormatJSON;
+    [mapper registerClass:[RKObjectMapperSpecUser class] forElementNamed:@"user"];
+    
+    [mapper mapObject:user fromString:[self arrayOfStringsJSON]];
+    NSArray* interests = [NSArray arrayWithObjects:@"basketball", @"soccer", nil];
+    [expectThat(user.interests) should:be(interests)];
 }
 
 - (void)itShouldNotUpdateNilPropertyToNil {
