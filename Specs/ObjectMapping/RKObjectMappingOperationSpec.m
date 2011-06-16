f2b62bb31e59b1f91442a3ed1de0071731692af2
Jeremy Ellison <jeremy@twotoasters.com>
2011-06-16 18:19:47 -0400

    Write a test that ensures Url's will evaluate equal properly and not get set if they already are.

--- Specs/ObjectMapping/RKObjectMappingOperationSpec.m
+++ Specs/ObjectMapping/RKObjectMappingOperationSpec.m
@@ -8,6 +8,20 @@
 
 #import "RKSpecEnvironment.h" 
 
+@interface TestMappable : NSObject {
+    NSURL* _url;
+}
+
+@property (nonatomic, retain) NSURL* url;
+
+@end
+
+@implementation TestMappable
+
+@synthesize url = _url;
+
+@end
+
 @interface RKObjectMappingOperationSpec : RKSpec {
     
 }
@@ -16,5 +30,21 @@
 
 @implementation RKObjectMappingOperationSpec
 
+- (void)itShouldNotUpdateEqualURLProperties {
+    RKObjectMapping* mapping = [RKObjectMapping mappingForClass:[TestMappable class]];
+    [mapping mapAttributes:@"url", nil];
+    NSURL* url1 = [NSURL URLWithString:@"http://www.restkit.org"];
+    NSURL* url2 = [NSURL URLWithString:@"http://www.restkit.org"];
+    assertThatBool(url1 == url2, is(equalToBool(NO)));
+    TestMappable* object = [[[TestMappable alloc] init] autorelease];
+    [object setUrl:nil];
+    NSDictionary* dictionary = [NSDictionary dictionaryWithObjectsAndKeys:url2, @"url", nil];
+    
+    RKObjectMappingOperation* operation = [[RKObjectMappingOperation alloc] initWithSourceObject:dictionary destinationObject:object objectMapping:mapping];
+    BOOL success = [operation performMapping:nil];
+    assertThatBool(success, is(equalToBool(YES)));
+    assertThatBool(object.url == url1, is(equalToBool(YES)));
+    [operation release];
+}
 
 @end
