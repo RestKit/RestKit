63762749004ae14f55a861e6932362f263573641
Blake Watters <blakewatters@gmail.com>
2011-07-06 15:26:44 -0400

    Fixes for URL encoding of paramters in GET request. closes #193

--- Code/Network/RKURL.h
+++ Code/Network/RKURL.h
@@ -9,12 +9,16 @@
 @interface RKURL : NSURL {
 	NSString* _baseURLString;
 	NSString* _resourcePath;
+	NSDictionary* _queryParams;
 }
 
 @property (nonatomic, readonly) NSString* baseURLString;
 @property (nonatomic, readonly) NSString* resourcePath;
+@property (nonatomic, readonly) NSDictionary* queryParams;
 
 - (id)initWithBaseURLString:(NSString*)baseURLString resourcePath:(NSString*)resourcePath;
+- (id)initWithBaseURLString:(NSString*)baseURLString resourcePath:(NSString*)resourcePath queryParams:(NSDictionary*)queryParams;
 + (RKURL*)URLWithBaseURLString:(NSString*)baseURLString resourcePath:(NSString*)resourcePath;
++ (RKURL*)URLWithBaseURLString:(NSString*)baseURLString resourcePath:(NSString*)resourcePath queryParams:(NSDictionary*)queryParams;
 
 @end
