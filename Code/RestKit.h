aae7a7f6420ec0e17252be999359db373ff93931
Parker <parkerboundy@gmail.com>
2011-09-07 10:34:26 -0400

    added blank comment to the RestKit.h header
    Added another blank comment row to the header to keep with the previous
    comment styles.

--- Code/RestKit.h
+++ Code/RestKit.h
@@ -16,6 +16,7 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+//
 
 #import "Network/Network.h"
 #import "Support/Support.h"
