d5987f043318cfd73d35d1c6f9d96415eda5352c
Blake Watters <blakewatters@gmail.com>
2011-10-11 09:21:08 -0400

    Workaround for crashes due to over-release of RKParams on iOS 5. fixes #392

--- Code/Network/RKParams.m
+++ Code/Network/RKParams.m
@@ -3,7 +3,7 @@
 //  RestKit
 //
 //  Created by Blake Watters on 8/3/09.
-//  Copyright 2009 Two Toasters
+//  Copyright 2009 RestKit
 //  
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -21,6 +21,11 @@
 #import "RKParams.h"
 #import "../Support/RKLog.h"
 #import "NSString+MD5.h"
+
+// Need for iOS 5 UIDevice workaround
+#if TARGET_OS_IPHONE
+#import <UIKit/UIKit.h>
+#endif
 
 // Set Logging Component
 #undef RKLogComponent
@@ -222,7 +227,12 @@ NSString* const kRKStringBoundary = @"0xKhTmLbOuNdArY";
         // NOTE: When we are assigned to the URL request, we get
         // retained. We release ourselves here to ensure the retain
         // count will hit zero after upload is complete.
-        [self release];
+        //
+        // This behavior does not seem to happen on iOS 5. This is a workaround until
+        // the problem can be analyzed in more detail
+        if ([[[UIDevice currentDevice] systemVersion] compare:@"5.0" options:NSNumericSearch] == NSOrderedAscending) {
+            [self release];
+        }
     }
 }
 
