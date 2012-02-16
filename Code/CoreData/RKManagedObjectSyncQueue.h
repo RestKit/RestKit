187b681acba999e5123060fe9b4009212a2f4359
ecordell <cordell.evan@gmail.com>
2012-02-16 12:34:34 -0600

    Adds a syncQueue entity to the object model.

---
+++ Code/CoreData/RKManagedObjectSyncQueue.h
@@ -0,0 +1,17 @@
+//
+//  RKManagedObjectSyncQueue.h
+//  RestKit
+//
+//  Created by Evan Cordell on 2/16/12.
+//  Copyright (c) 2012 RestKit. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface RKManagedObjectSyncQueue : NSManagedObject
+
+@property (nonatomic, retain) NSNumber * queuePosition;
+@property (nonatomic, retain) NSNumber * syncStatus;
+@property (nonatomic, retain) NSString * objectIDString;
+
+@end
