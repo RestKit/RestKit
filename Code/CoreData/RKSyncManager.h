f017cdf686690e24a775695896b85a40141c01b9
ecordell <cordell.evan@gmail.com>
2012-02-26 17:27:05 -0600

    Documentation

--- Code/CoreData/RKSyncManager.h
+++ Code/CoreData/RKSyncManager.h
@@ -3,7 +3,19 @@
 //  RestKit
 //
 //  Created by Evan Cordell on 2/16/12.
-//  Copyright (c) 2012 RestKit. All rights reserved.
+//  Copyright (c) 2012 RestKit.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//  
+//  http://www.apache.org/licenses/LICENSE-2.0
+//  
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
 //
 
 #import "RKObjectManager.h"
@@ -12,6 +24,9 @@
 #import "RKManagedObjectMapping.h"
 #import "RKManagedObjectMappingOperation.h"
 
+/**
+ Sync status types indicating HTTP method
+ */
 typedef enum {
     RKSyncStatusNone,
     RKSyncStatusPost,
@@ -22,9 +37,9 @@ typedef enum {
 @class RKSyncManager;
 
 /**
- * Notified of sync events for RKManagedObjectSyncObserver
+ * Notified of sync events for RKSyncManager
  */
-@protocol RKManagedObjectSyncDelegate <NSObject>
+@protocol RKSyncManagerDelegate <NSObject>
 @required
 
 /**
@@ -45,7 +60,8 @@ typedef enum {
 - (void)syncManager:(RKSyncManager *)syncManager didSyncWithSyncMode:(RKSyncMode)syncMode andClass:(Class)objectClass;
 
 /**
- * When implemented, sent before objects are pushed to the server.
+ * When implemented, sent before objects are pushed to the server. Objects in queue are of type RKManagedObjectSyncQueue. 
+ @see RKManagedObjectSyncQueue
  */
 - (void)syncManager:(RKSyncManager *)syncManager willPushObjectsInQueue:(NSArray *)queue withSyncMode:(RKSyncMode)syncMode andClass:(Class)objectClass;
 
@@ -67,23 +83,60 @@ typedef enum {
 
 @end
 
+/**
+ `RKSyncManager` handles the observation of Core Data changes and the syncronization of a local store with a remote server. This object is created automatically when an RKManagedObjectStore is initialized, and is associated with the objectStore's related `objectManager`.  
+ */
 @interface RKSyncManager : NSObject <RKObjectLoaderDelegate> {
     NSMutableArray *_queue;
 }
 
+/**
+ The objectManager that this syncManager is associated with.
+ @see RKObjectManager
+ */
 @property (nonatomic, readonly) RKObjectManager* objectManager;
-@property (nonatomic, assign) id<RKManagedObjectSyncDelegate> delegate;
+
+/**
+ The object that receives the RKManagedObjectSyncDelegate messages. 
+ @see RKManagedObjectSyncDelegate
+ */
+@property (nonatomic, assign) id<RKSyncManagerDelegate> delegate;
+
+/**
+ Creates an RKSyncManager and associates it with an RKObjectManager. This happens automatically on objectStore initialization, or one can be created seperately to handle different syncing tasks. Once initialized the `objectManager` is `readonly`.
+ */
 
 - (id)initWithObjectManager:(RKObjectManager*)objectManager;
 
+/**
+ Syncs (push followed by pull) all objects with a given syncMode and class. If syncMode is nil, it syncs all objects of type `objectClass`. If `objectClass` is nil, it syncs all objects with the specified `syncMode` regardless of the class. Both values cannot be nil.
+ */
 - (void)syncObjectsWithSyncMode:(RKSyncMode)syncMode andClass:(Class)objectClass;
+
+/**
+ Pushes all objects with a given syncMode and class to a remote server. If syncMode is nil, it pushes all objects of type `objectClass`. If `objectClass` is nil, it pushes all objects with the specified `syncMode` regardless of the class. Both values cannot be nil.
+ */
 - (void)pushObjectsWithSyncMode:(RKSyncMode)syncMode andClass:(Class)objectClass;
+
+/**
+ Pulls all objects with a given syncMode and class. If syncMode is nil, it pulls all objects of type `objectClass`. If `objectClass` is nil, it pulls all objects with the specified `syncMode` regardless of the class. Both values cannot be nil.
+ */
 - (void)pullObjectsWithSyncMode:(RKSyncMode)syncMode andClass:(Class)objectClass;
 
 
-//Shortcuts for syncing all classes set to RKSyncModeManual
+/**
+ Conveniance method for syncing objects with `syncMode = RKSyncModeManual`
+ */
 - (void)sync;
+
+/**
+ Conveniance method for pushing objects with `syncMode = RKSyncModeManual`
+ */
 - (void)push;
+
+/**
+ Conveniance method for pulling objects with `syncMode = RKSyncModeManual`
+ */
 - (void)pull;
 
 @end
