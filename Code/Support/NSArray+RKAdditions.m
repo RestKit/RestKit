76f2344582d87eaf097e90d573fe99947dc0f3b3
Blake Watters <blakewatters@gmail.com>
2012-04-10 14:29:20 -0400

    Replace implementation of sectionsGroupedByKeyPath: due to issues with valueForKeyPath: mutating ordering

--- Code/Support/NSArray+RKAdditions.m
+++ Code/Support/NSArray+RKAdditions.m
@@ -12,18 +12,46 @@
 
 - (NSArray *)sectionsGroupedByKeyPath:(NSString *)keyPath
 {
-    NSString *keyPathWithOperator = [NSString stringWithFormat:@"@distinctUnionOfObjects.%@", keyPath];
-    NSArray *sectionValues = [self valueForKeyPath:keyPathWithOperator];
-    NSMutableArray *sections = [NSMutableArray arrayWithCapacity:[sectionValues count]];
-    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"%K = $sectionValue", keyPath];
-    for (id value in sectionValues) {
-        NSDictionary *sub = [NSDictionary dictionaryWithObject:value forKey:@"sectionValue"];
-        NSPredicate *sectionPredicate = [predicate predicateWithSubstitutionVariables:sub];
-        NSArray *objectsForSection = [self filteredArrayUsingPredicate:sectionPredicate];
-        [sections addObject:objectsForSection];
+    // Code adapted from: https://gist.github.com/1243312
+    NSMutableArray *sections = [NSMutableArray array];
+
+	// If we don't contain any items, return an empty collection of sections.
+	if([self count] == 0) {
+		return sections;
+    }
+
+	// Create the first section and establish the first section's grouping value.
+	NSMutableArray *sectionItems = [NSMutableArray array];
+	id currentGroup = [[self objectAtIndex:0] valueForKeyPath:keyPath];
+
+	// Iterate over our items, placing them in the appropriate section and
+	// creating new sections when necessary.
+	for (id item in self) {
+		// Retrieve the grouping value from the current item.
+		id itemGroup = [item valueForKeyPath:keyPath];
+
+		// Compare the current item's grouping value to the current section's
+		// grouping value.
+		if (![itemGroup isEqual:currentGroup]) {
+			// The current item doesn't belong in the current section, so
+			// store the section we've been building and create a new one,
+			// caching the new grouping value.
+			[sections addObject:sectionItems];
+			sectionItems = [NSMutableArray array];
+			currentGroup = itemGroup;
+		}
+
+		// Add the item to the appropriate section.
+		[sectionItems addObject:item];
+	}
+
+	// If we were adding items to a section that has not yet been added
+	// to the aggregate section collection, add it now.
+	if ([sectionItems count] > 0) {
+		[sections addObject:sectionItems];
     }
     
-    return [NSArray arrayWithArray:sections];
+	return sections;
 }
 
 @end
