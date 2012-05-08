a1fd9a651494ab1e17f43017d0e0658d9f2740bb
Russ Bishop <russ@MacbookPro.local>
2012-05-07 19:17:55 -0500

    Added method to let RestKit do the path parsing for retrieving a fetch controller to reduce boilerplate

--- Code/CoreData/RKObjectMappingProvider+CoreData.m
+++ Code/CoreData/RKObjectMappingProvider+CoreData.m
@@ -9,20 +9,42 @@
 #import "RKObjectMappingProvider+CoreData.h"
 #import "RKOrderedDictionary.h"
 #import "RKFixCategoryBug.h"
+#import "RKPathMatcher.h"
+#import <objc/runtime.h>
+
 
 RK_FIX_CATEGORY_BUG(RKObjectMappingProvider_CoreData)
 @implementation RKObjectMappingProvider (CoreData)
+
+const NSString *RK_OBJECT_MAPPING_PROVIDER_IS_SIMPLE_BLOCK = @"RKObjectMappingProvider_IsSimpleBlock";
+const NSString *RK_OBJECT_MAPPING_PROVIDER_RESOURCE_PATTERN = @"RKObjectMappingProvider_ResourcePattern";
 
 - (void)setObjectMapping:(RKObjectMappingDefinition *)objectMapping forResourcePathPattern:(NSString *)resourcePath withFetchRequestBlock:(RKObjectMappingProviderFetchRequestBlock)fetchRequestBlock {
     [self setEntry:[RKObjectMappingProviderContextEntry contextEntryWithMapping:objectMapping
                                                                        userData:fetchRequestBlock] forResourcePathPattern:resourcePath];
 }
 
+- (void)setObjectMapping:(RKObjectMappingDefinition *)objectMapping forResourcePathPattern:(NSString *)resourcePath withSimpleFetchRequestBlock:(RKObjectMappingProviderSimpleFetchRequestBlock)fetchRequestBlock {
+    objc_setAssociatedObject(fetchRequestBlock, RK_OBJECT_MAPPING_PROVIDER_IS_SIMPLE_BLOCK, [NSNumber numberWithBool:YES], OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    objc_setAssociatedObject(fetchRequestBlock, RK_OBJECT_MAPPING_PROVIDER_RESOURCE_PATTERN, resourcePath, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    [self setEntry:[RKObjectMappingProviderContextEntry contextEntryWithMapping:objectMapping userData:fetchRequestBlock] forResourcePathPattern:resourcePath];
+}
+
 - (NSFetchRequest *)fetchRequestForResourcePath:(NSString *)resourcePath {
     RKObjectMappingProviderContextEntry *entry = [self entryForResourcePath:resourcePath];
     if (entry.userData) {
-        NSFetchRequest *(^fetchRequestBlock)(NSString *) = entry.userData;
-        return fetchRequestBlock(resourcePath);
+        BOOL isSimpleBlock = [objc_getAssociatedObject(entry.userData, RK_OBJECT_MAPPING_PROVIDER_IS_SIMPLE_BLOCK) boolValue];
+        if(isSimpleBlock) {
+            NSFetchRequest *(^simpleFetchRequestBlock)(NSDictionary*) = entry.userData;
+            NSString *resourcePattern = objc_getAssociatedObject(entry.userData, RK_OBJECT_MAPPING_PROVIDER_RESOURCE_PATTERN);
+            NSDictionary *parsed = nil;
+            RKPathMatcher *matcher = [RKPathMatcher matcherWithPath:resourcePath];
+            [matcher matchesPattern:resourcePattern tokenizeQueryStrings:YES parsedArguments:&parsed];
+            return simpleFetchRequestBlock(parsed);
+        } else {
+            NSFetchRequest *(^fetchRequestBlock)(NSString *) = entry.userData;
+            return fetchRequestBlock(resourcePath);
+        }
     }
 
     return nil;
