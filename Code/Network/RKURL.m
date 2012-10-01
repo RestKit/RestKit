999f50643a8ad1569c3bd745ba1d2bcfe83db47f
Romain Champourlier <pro@rchampourlier.com>
2012-10-01 15:38:13 +0200

    Corrected RKURL#URLByAppendingQueryParameters: was clearning URL's resource path
    When creating a new RKURL through URLByAppendingQueryParameters, the resourcePath
    was set to nil.

--- Code/Network/RKURL.m
+++ Code/Network/RKURL.m
@@ -130,7 +130,7 @@
 
 - (RKURL *)URLByAppendingQueryParameters:(NSDictionary *)theQueryParameters
 {
-    return [RKURL URLWithBaseURL:self resourcePath:nil queryParameters:theQueryParameters];
+    return [RKURL URLWithBaseURL:self resourcePath:resourcePath queryParameters:theQueryParameters];
 }
 
 - (RKURL *)URLByReplacingResourcePath:(NSString *)newResourcePath
