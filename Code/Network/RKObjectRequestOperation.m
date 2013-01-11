f1a35bb8849653fa7aa64f2f4efd1aad19bd2303
Blake Watters <blakewatters@gmail.com>
2013-01-10 19:54:18 -0500

    Style tweak

--- Code/Network/RKObjectRequestOperation.m
+++ Code/Network/RKObjectRequestOperation.m
@@ -125,9 +125,11 @@ static NSString *RKStringDescribingURLResponseWithData(NSURLResponse *response, 
 - (void)dealloc
 {
 #if !OS_OBJECT_USE_OBJC
-    if(_failureCallbackQueue) dispatch_release(_failureCallbackQueue);
-    if(_successCallbackQueue) dispatch_release(_successCallbackQueue);
+    if (_failureCallbackQueue) dispatch_release(_failureCallbackQueue);
+    if (_successCallbackQueue) dispatch_release(_successCallbackQueue);
 #endif
+    _failureCallbackQueue = NULL;
+    _successCallbackQueue = NULL;
 }
 
 // Designated initializer
