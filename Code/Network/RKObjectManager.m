7cca6891cefd8f3e405cc2a438d25a8a5cba44dd
Blake Watters <blakewatters@gmail.com>
2013-01-23 17:28:08 -0500

    Unwind earlier fix for obtaining permanent objectID for targetObject in favor of the overridden setter on RKManagedObjectRequestOperation

--- Code/Network/RKObjectManager.m
+++ Code/Network/RKObjectManager.m
@@ -533,8 +533,7 @@ static NSString *RKMIMETypeFromAFHTTPClientParameterEncoding(AFHTTPClientParamet
         if ([object isKindOfClass:[NSManagedObject class]]) {
             static NSPredicate *temporaryObjectsPredicate = nil;
             if (! temporaryObjectsPredicate) temporaryObjectsPredicate = [NSPredicate predicateWithFormat:@"objectID.isTemporaryID == YES"];
-            NSSet *potentiallyTemporaryObjects = (object && [[object objectID] isTemporaryID]) ? [[managedObjectContext insertedObjects] setByAddingObject:object] : [managedObjectContext insertedObjects];
-            NSSet *temporaryObjects = [potentiallyTemporaryObjects filteredSetUsingPredicate:temporaryObjectsPredicate];
+            NSSet *temporaryObjects = [[managedObjectContext insertedObjects] filteredSetUsingPredicate:temporaryObjectsPredicate];
             if ([temporaryObjects count]) {
                 RKLogInfo(@"Asked to perform object request for NSManagedObject with temporary object IDs: Obtaining permanent ID before proceeding.");
                 __block BOOL _blockSuccess;
