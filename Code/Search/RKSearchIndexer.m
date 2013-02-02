ae8e3b17ca48756cc9cf06f9f58004af41523d08
Blake Watters <blakewatters@gmail.com>
2013-02-02 09:25:02 -0500

    Relax assertion in `RKSearchIndexer` so that it does not fail in the event a `nil` managed object is returned by `existingObjectWithID:error:`

--- Code/Search/RKSearchIndexer.m
+++ Code/Search/RKSearchIndexer.m
@@ -311,7 +311,7 @@ NSString * const RKSearchableAttributeNamesUserInfoKey = @"RestKitSearchableAttr
             [self.indexingContext performBlockAndWait:^{
                 NSError *error = nil;
                 NSManagedObject *managedObject = [self.indexingContext existingObjectWithID:objectID error:&error];
-                NSAssert([[managedObject managedObjectContext] isEqual:self.indexingContext], @"Serious Core Data error: Asked for an `NSManagedObject` with ID %@ in indexing context %@, but got one in %@", objectID, self.indexingContext, [managedObject managedObjectContext]);
+                NSAssert(managedObject == nil || [[managedObject managedObjectContext] isEqual:self.indexingContext], @"Serious Core Data error: Asked for an `NSManagedObject` with ID %@ in indexing context %@, but got one in %@", objectID, self.indexingContext, [managedObject managedObjectContext]);
                 if (managedObject && error == nil) {
                     BOOL performIndexing = YES;
                     if ([self.delegate respondsToSelector:@selector(searchIndexer:shouldIndexManagedObject:)]) {
