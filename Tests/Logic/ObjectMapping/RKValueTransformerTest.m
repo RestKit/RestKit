a5781ad8b852f3583c83591250ed992694ac15e4
Samuel E. Giddins <segiddins@segiddins.me>
2013-06-13 10:49:59 -0400

    Add RKValueTransformer Tests

---
+++ Tests/Logic/ObjectMapping/RKValueTransformerTest.m
@@ -0,0 +1,62 @@
+//
+//  RKValueTransformerTest.m
+//  RestKit
+//
+//  Created by Samuel E. Giddins on 6/13/13.
+//  Copyright (c) 2013 RestKit. All rights reserved.
+//
+
+#import "RKTestEnvironment.h"
+#import "RKValueTransformers.h"
+
+@interface RKValueTransformerTest : SenTestCase
+
+@end
+
+@implementation RKValueTransformerTest
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+    [RKValueTransformer class];
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testValueTransformerRequiresAllParametersExceptReverse
+{
+    Class source = nil, destination = nil;
+    RKValueTransformationBlock forward = nil, reverse = nil;
+    source = [NSString class];
+    destination = [NSURL class];
+    forward = ^BOOL(id inputValue, id *outputValue, NSError **error) {
+        *outputValue = [NSURL URLWithString:inputValue];
+        return YES;
+    };
+    reverse = ^BOOL(id inputValue, id *outputValue, NSError **error) {
+        *outputValue = [inputValue absoluteString];
+        return YES;
+    };
+    
+    // All params OK
+    expect(^{[RKValueTransformer valueTransformerWithSourceClass:source destinationClass:destination transformationBlock:forward reverseTransformationBlock:reverse];}).toNot.raiseAny();
+    
+    // nil reverse passes
+    expect(^{[RKValueTransformer valueTransformerWithSourceClass:source destinationClass:destination transformationBlock:forward reverseTransformationBlock:nil];}).toNot.raiseAny();
+    
+    // nil transformationBlock fails
+    expect(^{[RKValueTransformer valueTransformerWithSourceClass:source destinationClass:destination transformationBlock:nil reverseTransformationBlock:reverse];}).to.raise(NSInvalidArgumentException);
+    
+    // nil source fails
+    expect(^{[RKValueTransformer valueTransformerWithSourceClass:nil destinationClass:destination transformationBlock:forward reverseTransformationBlock:reverse];}).to.raise(NSInvalidArgumentException);
+    
+    // nil destination fails
+    expect(^{[RKValueTransformer valueTransformerWithSourceClass:source destinationClass:nil transformationBlock:forward reverseTransformationBlock:reverse];}).to.raise(NSInvalidArgumentException);
+}
+
+@end
