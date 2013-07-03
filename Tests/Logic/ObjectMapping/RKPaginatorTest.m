148f7c66f37eec54f47f683fad2305f2e273343f
Jeremy Lyman <jeremy@lostgateway.com>
2013-07-03 01:23:37 -0500

    Added test to determine whether an assertion is thrown while load is called while another is still processing. Added test to determine if the objectRequestOperation is populated after loading. Added test to determine if the objectRequestOperation from one load: is different after the next load: method is called.

--- Tests/Logic/ObjectMapping/RKPaginatorTest.m
+++ Tests/Logic/ObjectMapping/RKPaginatorTest.m
@@ -440,4 +440,42 @@ static NSString * const RKPaginatorTestResourcePathPatternWithOffset = @"/pagina
     expect(paginator.offset).to.equal(3);
 }
 
+- (void)testLoadingAPageWhileOtherPageLoads
+{
+    NSURLRequest *request = [NSURLRequest requestWithURL:self.paginationURL];
+    NSException *exception = nil;
+    @try {
+        RKPaginator *paginator = [[RKPaginator alloc] initWithRequest:request paginationMapping:self.paginationMapping responseDescriptors:@[ self.responseDescriptor ]];
+        [paginator loadPage:1];
+        [paginator loadPage:2];
+    }
+    @catch (NSException *e) {
+        exception = e;
+    }
+    expect(exception).notTo.beNil();
+    expect([exception reason]).to.equal(@"Cannot perform a load while one is already in progress.");
+}
+
+- (void)testHavingRequestOperationUponCompletion
+{
+    NSURLRequest *request = [NSURLRequest requestWithURL:self.paginationURL];
+    RKPaginator *paginator = [[RKPaginator alloc] initWithRequest:request paginationMapping:self.paginationMapping responseDescriptors:@[ self.responseDescriptor ]];
+    [paginator loadPage:1];
+    [paginator waitUntilFinished];
+    expect(paginator.objectRequestOperation).willNot.beNil();
+}
+
+- (void)testChangeOfRequestOperationOnSubsequentRequests
+{
+    NSURLRequest *request = [NSURLRequest requestWithURL:self.paginationURL];
+    RKObjectRequestOperation *operation = nil;
+    RKPaginator *paginator = [[RKPaginator alloc] initWithRequest:request paginationMapping:self.paginationMapping responseDescriptors:@[ self.responseDescriptor ]];
+    [paginator loadPage:1];
+    [paginator waitUntilFinished];
+    operation = paginator.objectRequestOperation;
+    [paginator loadPage:2];
+    [paginator waitUntilFinished];
+    expect(paginator.objectRequestOperation).willNot.equal(operation);
+}
+
 @end
