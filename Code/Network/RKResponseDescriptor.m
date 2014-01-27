cf0f58620d130897ad8fc506fefa461aa829069b
Alex Leigh <alex@revinate.com>
2014-01-27 15:48:12 -0800

    Simplify testing for pathPattern and keyPath equality per segiddins' suggestion.

--- Code/Network/RKResponseDescriptor.m
+++ Code/Network/RKResponseDescriptor.m
@@ -167,8 +167,8 @@ extern NSString *RKStringDescribingRequestMethod(RKRequestMethod method);
     return
     [self.mapping isEqualToMapping:otherDescriptor.mapping] &&
     self.method == otherDescriptor.method &&
-    ((self.pathPattern == nil && otherDescriptor.pathPattern == nil) || [self.pathPattern isEqualToString:otherDescriptor.pathPattern]) &&
-    ((self.keyPath == nil && otherDescriptor.keyPath == nil) || [self.keyPath isEqualToString:otherDescriptor.keyPath]) &&
+    ((self.pathPattern == otherDescriptor.pathPattern) || [self.pathPattern isEqualToString:otherDescriptor.pathPattern]) &&
+    ((self.keyPath == otherDescriptor.keyPath) || [self.keyPath isEqualToString:otherDescriptor.keyPath]) &&
     [self.statusCodes isEqualToIndexSet:otherDescriptor.statusCodes];
 }
 
