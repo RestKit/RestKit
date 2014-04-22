db7dee4ea40413510bbb212e1f3d9fb1dd603cc3
Lubbo <davide.ramo.ve@gmail.com>
2014-04-22 13:44:11 +0200

    Mapping Values without Key Paths: CoreData Problem
    when attributeMapping.sourceKeyPath == nil
    value is a RKMappingSourceObject instance
    This cause CoreData to fail on predicate that uses the object
    description redefined in RKMappingSourceObject

--- Code/ObjectMapping/RKMappingOperation.m
+++ Code/ObjectMapping/RKMappingOperation.m
@@ -508,7 +508,7 @@ static NSString *const RKRootKeyPathPrefix = @"@root.";
             continue;
         }
 
-        id value = (attributeMapping.sourceKeyPath == nil) ? self.sourceObject : [self.sourceObject valueForKeyPath:attributeMapping.sourceKeyPath];
+        id value = (attributeMapping.sourceKeyPath == nil) ? [self.sourceObject valueForKeyPath:@"self"] : [self.sourceObject valueForKeyPath:attributeMapping.sourceKeyPath];
         if ([self applyAttributeMapping:attributeMapping withValue:value]) {
             appliedMappings = YES;
         } else {
