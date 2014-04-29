a7d221f61e43ac760a17611bcd7d049abe00b7cc
Samuel E. Giddins <segiddins@segiddins.me>
2014-04-28 23:05:09 -0500

    Make RestKit.h importable with standalone subspecs

--- Code/RestKit.h
+++ Code/RestKit.h
@@ -21,9 +21,17 @@
 #ifndef _RESTKIT_
 #define _RESTKIT_
 
+#if __has_include("ObjectMapping.h")
 #import "ObjectMapping.h"
+#endif
+
+#if __has_include("Network.h")
 #import "Network.h"
+#endif
+
+#if __has_include("Support.h")
 #import "Support.h"
+#endif
 
 #if __has_include("RKCoreData.h")
 #import "RKCoreData.h"
