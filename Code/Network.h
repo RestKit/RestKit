8ef985533675884a0b0e796bbf3ed38df62675a4
Daniel Phillips <daniel@futureworkshops.com>
2014-05-08 10:10:01 +0100

    Issue #1912: RKPathMatcher not imported

--- Code/Network.h
+++ Code/Network.h
@@ -27,6 +27,7 @@
 #import "RKHTTPUtilities.h"
 #import "RKObjectRequestOperation.h"
 #import "RKObjectParameterization.h"
+#import "RKPathMatcher.h"
 
 #ifdef _COREDATADEFINES_H
 #import "RKManagedObjectRequestOperation.h"
