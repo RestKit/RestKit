d92b6bfde25f48009f9ead627b424e19d4b979b5
Karl Gluck <kgluck@curioushq.com>
2014-07-02 09:50:59 -0500

    Build the RKPathMatcher for a RKResponseDescriptor only once
    Creating and destroying the path matcher becomes very slow when a project has hundreds of response descriptors

--- Code/Network/RKResponseDescriptor.m
+++ Code/Network/RKResponseDescriptor.m
@@ -63,6 +63,7 @@ extern NSString *RKStringDescribingRequestMethod(RKRequestMethod method);
 @property (nonatomic, strong, readwrite) RKMapping *mapping;
 @property (nonatomic, assign, readwrite) RKRequestMethod method;
 @property (nonatomic, copy, readwrite) NSString *pathPattern;
+@property (nonatomic, strong, readwrite) RKPathMatcher *pathPatternMatcher;
 @property (nonatomic, copy, readwrite) NSString *keyPath;
 @property (nonatomic, copy, readwrite) NSIndexSet *statusCodes;
 @end
@@ -97,6 +98,16 @@ extern NSString *RKStringDescribingRequestMethod(RKRequestMethod method);
     return mappingDescriptor;
 }
 
+- (void)setPathPattern:(NSString *)pathPattern
+{
+    _pathPattern = pathPattern;
+    if (pathPattern) {
+        self.pathPatternMatcher = [RKPathMatcher pathMatcherWithPattern:pathPattern];
+    } else {
+        self.pathPatternMatcher = nil;
+    }
+}
+
 - (NSString *)description
 {
     return [NSString stringWithFormat:@"<%@: %p method=%@ pathPattern=%@ keyPath=%@ statusCodes=%@ : %@>",
@@ -106,8 +117,7 @@ extern NSString *RKStringDescribingRequestMethod(RKRequestMethod method);
 - (BOOL)matchesPath:(NSString *)path
 {
     if (!self.pathPattern || !path) return YES;
-    RKPathMatcher *pathMatcher = [RKPathMatcher pathMatcherWithPattern:self.pathPattern];
-    return [pathMatcher matchesPath:path tokenizeQueryStrings:NO parsedArguments:nil];
+    return [self.pathPatternMatcher matchesPath:path tokenizeQueryStrings:NO parsedArguments:nil];
 }
 
 - (BOOL)matchesURL:(NSURL *)URL
