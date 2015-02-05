087fbce7f2a681af1f1c9457733a818232d11628
Michał Hernas <michal@hernas.pl>
2015-02-05 19:56:43 +0100

    Some more cleanup

--- Tests/RKObjectRequestOperationTestForSwift.swift
+++
@@ -1,9 +0,0 @@
-//
-//  RKObjectRequestOperationTestForSwift.swift
-//  RestKit
-//
-//  Created by Michał Hernas on 05/02/15.
-//  Copyright (c) 2015 RestKit. All rights reserved.
-//
-
-import Foundation
