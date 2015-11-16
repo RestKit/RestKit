94fd35104ef6c95c0a882bc095d0f122866d6630
Alex Antonyuk <alex@antonyuk.me>
2015-11-16 18:59:14 +0100

    complete fix for include checks
    Fix for #2352

--- Code/Network/RKManagedObjectRequestOperation.h
+++ Code/Network/RKManagedObjectRequestOperation.h
@@ -19,7 +19,7 @@
 //
 
 #ifdef _COREDATADEFINES_H
-#if __has_include("RKManagedObjectCaching.h")
+#if __has_include(<RestKit/CoreData/RKManagedObjectCaching.h>)
 
 #import <RestKit/CoreData/RKManagedObjectCaching.h>
 #import <RestKit/Network/RKObjectRequestOperation.h>
