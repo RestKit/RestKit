170977abbb5ec362f21c2a244c550b6b99cb3b5f
Fred Cox <fred@ekreative.com>
2016-02-25 19:27:29 +0200

    Revert "Merge pull request #2355 from dev4dev/development"
    This reverts commit 95f501ab3a83a3c4f3d9e4bca720809d4997c3dd, reversing
    changes made to fb6d3df85532071f5f7d7542b09804e3e280c112.

---
+++ Code/CoreData/RKCoreData.h
@@ -0,0 +1,26 @@
+//
+//  RKCoreData.h
+//  RestKit
+//
+//  Created by Samuel E. Giddins on 4/23/14.
+//  Copyright (c) 2014 RestKit. All rights reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#ifndef RestKit_RKCoreData_h
+#define RestKit_RKCoreData_h
+
+#import <RestKit/CoreData.h>
+
+#endif
