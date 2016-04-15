9a8a1b04925a2b17596f216a37343784659fd3bf
kanunnikau <dkanunnikau@gmail.com>
2016-04-15 08:37:09 +0200

    RKHTTPRequestOperation: remove unused (and possibly dangerous) NSURLConnectionDelegate method. If it happens to be used it will try to trigger this method on a super - but no superclass implements it, so it will crash. RKMappingOperation: remove RestKit 'mappingOperationShouldSkipPropertyMapping:' deprecation warning by replacing with two corresponding methods (attribute, relationship mapping). Is it a suitable fix?

--- Code/ObjectMapping/RKMappingOperation.m
+++ Code/ObjectMapping/RKMappingOperation.m
@@ -1222,10 +1222,9 @@ static NSArray *RKInsertInMetadataList(NSArray *list, id metadata1, id metadata2
         }
     }
     
-    BOOL canSkipProperties = [dataSource respondsToSelector:@selector(mappingOperationShouldSkipPropertyMapping:)] && [dataSource mappingOperationShouldSkipPropertyMapping:self];
-    BOOL canSkipAttributes = canSkipProperties;
-    BOOL canSkipRelationships = canSkipProperties;
-    if ([dataSource respondsToSelector:@selector(mappingOperationShouldSkipRelationshipMapping:)]) {
+  BOOL canSkipAttributes = [dataSource respondsToSelector:@selector(mappingOperationShouldSkipAttributeMapping:)] && [dataSource mappingOperationShouldSkipAttributeMapping:self];
+  BOOL canSkipRelationships = [dataSource respondsToSelector:@selector(mappingOperationShouldSkipRelationshipMapping:)] && [dataSource mappingOperationShouldSkipRelationshipMapping:self];
+  if ([dataSource respondsToSelector:@selector(mappingOperationShouldSkipRelationshipMapping:)]) {
         canSkipRelationships = [dataSource mappingOperationShouldSkipRelationshipMapping:self];
     }
     if ([dataSource respondsToSelector:@selector(mappingOperationShouldSkipAttributeMapping:)]) {
