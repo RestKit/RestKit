9628336890389384206c3784bc226700a51cbbc5
Valerio Mazzeo <valeriomazzeo@users.noreply.github.com>
2016-07-07 10:16:57 +0100

    Update UIImageView+AFRKNetworking.h

--- Code/Network/AFNetworking/UIImageView+AFRKNetworking.h
+++ Code/Network/AFNetworking/UIImageView+AFRKNetworking.h
@@ -31,7 +31,7 @@
 /**
  This category adds methods to the UIKit framework's `UIImageView` class. The methods in this category provide support for loading remote images asynchronously from a URL.
  */
-@interface UIImageView (AFNetworking)
+@interface UIImageView (AFRKNetworking)
 
 /**
  Creates and enqueues an image request operation, which asynchronously downloads the image from the specified URL, and sets it the request is finished. Any previous image request for the receiver will be cancelled. If the image is cached locally, the image is set immediately, otherwise the specified placeholder image will be set immediately, and then the remote image will be set once the request is finished.
