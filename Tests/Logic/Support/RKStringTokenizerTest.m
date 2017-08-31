66ee304a2b94b235e09c12b5abb3921e5a460815
Simo ++ <1262637+SimonMcFarlane@users.noreply.github.com>
2017-08-31 20:59:12 +0100

    Changed RKStringTokenizer to allow tokenization of symbol characters (#2516)
    * Changed the `RKStringTokenizer` method `- (NSSet *)tokenize:(NSString *)string` to use `kCFStringTokenizerUnitWordBoundary` instead of `kCFStringTokenizerUnitWord`. This then allows tokens to be created for symbols such as '@', '$' and '#' and therefore making them searchable.
    
    As a result of using `kCFStringTokenizerUnitWordBoundary` instead of `kCFStringTokenizerUnitWord`, spaces are detected and a token created. As we don't want to be returning results that include a space, this token (if it exists) is removed before the tokens are returned.
    
    Unit tests added to cover both the creating of tokens from strings that contain symbols and searching for managed objects that contain symbols.
    
    * Core formatting - removed tabs from empty lines in RKStringTokenizer.
    
    * Fixed assert mistake in RKSearchTest unit test `testSearchingForManagedObjectsWithSymbols`.

--- Tests/Logic/Support/RKStringTokenizerTest.m
+++ Tests/Logic/Support/RKStringTokenizerTest.m
@@ -23,12 +23,29 @@
     expect(tokens).to.equal(expectedTokens);
 }
 
+- (void)testTokenizingStringWithSymbols
+{
+    RKStringTokenizer *stringTokenizer = [RKStringTokenizer new];
+    NSSet *tokens = [stringTokenizer tokenize:@"This is a symbol test! $"];
+    NSSet *expectedTokens = [NSSet setWithArray:@[ @"this", @"is", @"a", @"symbol", @"test", @"!", @"$" ]];
+    expect(tokens).to.equal(expectedTokens);
+}
+
 - (void)testTokenizingStringWithStopWords
 {
     RKStringTokenizer *stringTokenizer = [RKStringTokenizer new];
     stringTokenizer.stopWords = [NSSet setWithObjects:@"is", @"a", nil];
-    NSSet *tokens = [stringTokenizer tokenize:@"This is a test"];
-    NSSet *expectedTokens = [NSSet setWithArray:@[ @"this", @"test" ]];
+    NSSet *tokens = [stringTokenizer tokenize:@"This is a stop word test"];
+    NSSet *expectedTokens = [NSSet setWithArray:@[ @"this", @"stop", @"word", @"test" ]];
+    expect(tokens).to.equal(expectedTokens);
+}
+
+- (void)testTokenizingStringWithStopWordsAndSymbols
+{
+    RKStringTokenizer *stringTokenizer = [RKStringTokenizer new];
+    stringTokenizer.stopWords = [NSSet setWithObjects:@"is", @"a", @"!", @"%",  nil];
+    NSSet *tokens = [stringTokenizer tokenize:@"This is a stop word symbol test! # %"];
+    NSSet *expectedTokens = [NSSet setWithArray:@[ @"this", @"stop", @"word", @"symbol", @"test", @"#" ]];
     expect(tokens).to.equal(expectedTokens);
 }
 
