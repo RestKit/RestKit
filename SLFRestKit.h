b6892895d6273fecffdf333cf0f3d0b6cf472d86
Greg Combs <gcombs@gmail.com>
2017-09-14 16:13:27 -0500

    Initial commit

---
+++ SLFRestKit.h
@@ -0,0 +1,25 @@
+//
+//  SLFRestKit.h
+//  SLFRestKit
+//
+//  Created by Gregory Combs on 5/17/16.
+//  Copyright © 2016 RestKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SLFRestKit.
+FOUNDATION_EXPORT double SLFRestKitVersionNumber;
+
+//! Project version string for SLFRestKit.
+FOUNDATION_EXPORT const unsigned char SLFRestKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLFRestKit/PublicHeader.h>
+
+#import <SLFRestKit/RestKit.h>
+#import <SLFRestKit/Network.h>
+#import <SLFRestKit/Support.h>
+#import <SLFRestKit/CoreData.h>
+#import <SLFRestKit/ObjectMapping.h>
+#import <SLFRestKit/UI.h>
+#import <SLFRestKit/JSONKit.h>
