774aa345a53d7ae9c8a7d8dc4567035fb5fe51bd
Tyler Milner <tyler.milner@bottlerocketstudios.com>
2018-03-02 19:24:55 -0600

    Adopted library testing syntax and stubbed out a couple of extra tests. The test is still failing at this point since I haven't yet fixed the bug.

--- Tests/Logic/Network/AFRKNetworkingTests.m
+++ Tests/Logic/Network/AFRKNetworkingTests.m
@@ -6,7 +6,7 @@
 //  Copyright © 2018 RestKit. All rights reserved.
 //
 
-#import <XCTest/XCTest.h>
+#import "RKTestEnvironment.h"
 #import "UIImageView+AFRKNetworking.h"
 
 // Re-defining the interface for AFRKImageCache since it's only privately declared in UIImageView+AFRKNetworking.m.
@@ -27,31 +27,60 @@
 
 @implementation AFRKNetworkingTests
 
+#pragma mark - Tests
+
 - (void)testSetImageWithURLReturnsURLRequestInCompletionBlockWhenImageIsFetchedFromCache
 {
-    // Arrange
+    [self performMockCachedSetImageWithURLRequestCallWithSuccessAssertion:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
+        expect(request).toNot.equal(nil);
+    } failureAssertion:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
+        failure(@"image request should always succeed for this test");
+    }];
+}
+
+- (void)testSetImageWithURLReturnsNilURLResponseInCompletionBlockWhenImageIsFetchedFromCache
+{
+    
+}
+
+- (void)testSetImageWithURLReturnsImageInCompletionBlockWhenImageIsFetchedFromCache
+{
+    
+}
+
+#pragma mark - Private
+
+- (void)performMockCachedSetImageWithURLRequestCallWithSuccessAssertion:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image))successAssertion failureAssertion:(void (^)(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error))failureAssertion
+{
+    // Act
+    // Create dummy request and image
     NSURL *imageURL = [[NSURL alloc] initWithString:@"https://test.com/image.png"];
     NSURLRequest *request = [[NSURLRequest alloc] initWithURL:imageURL];
     
     UIImage *testImage = [[UIImage alloc] init];
     
+    // Side-load the image into the cache
     AFRKImageCache *cache = [UIImageView afrk_sharedImageCache];
     [cache cacheImage:testImage forRequest:request];
     
+    // Setup the System Under Test
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectZero];
     
     // Act
+    // Call 'setImageWithURLRequest:placeholderImage:success:failure:'
+    // Run the provided assertion blocks inside of their respective completion blocks
     XCTestExpectation *asyncExpectation = [self expectationWithDescription:@"image view loads"];
     
     [imageView setImageWithURLRequest:request placeholderImage:nil success:^(NSURLRequest *request, NSHTTPURLResponse *response, UIImage *image) {
-        XCTAssertNotNil(request);
+        successAssertion(request, response, image);
         [asyncExpectation fulfill];
     } failure:^(NSURLRequest *request, NSHTTPURLResponse *response, NSError *error) {
-        XCTFail("%@ should succeed", NSStringFromSelector(@selector(setImageWithURLRequest:placeholderImage:success:failure:)));
+        failureAssertion(request, response, error);
         [asyncExpectation fulfill];
     }];
     
     // Assert
+    // Assertion blocks should have been run
     [self waitForExpectationsWithTimeout:0.5 handler:nil];
 }
 
